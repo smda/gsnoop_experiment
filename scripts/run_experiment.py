8ad0c567f067240c6077c34f0801453a3170252a
Stefan Mühlbauer <muehlbauer@informatik.uni-leipzig.de>
2024-05-13 13:19:38 +0200

    last changes

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -6,7 +6,7 @@ import numpy as np
 import json
 import pandas as pd
 
-from gsnoop.util import diff_transform, xor_transform, precision, recall, f1_score
+from gsnoop.util import diff_transform, xor_transform, precision, recall, f1
 from gsnoop.causal import find_hitting_set, find_greedy_hitting_set
 from gsnoop.screening import group_screening, lasso_screening
 
@@ -15,56 +15,95 @@ from .scripts.config import REPETITIONS
 # no random here
 np.random.seed(1)
 
-# retrieve task id from args
-index = 0
 
-# TODO retrieve
-relevant_options = []
+def main(index):
+    with open("./build/oracles.json", "r") as f:
+        metadata = json.load(f)
 
-# TODO retrieve and compute metadata sucha s absolute feature size
+    with open(f"./build/params/{index}.json", "r") as f:
+        params = json.load(f)
 
-# retrieve metadata
-with open(f"./build/params/{index}.json", "r") as f:
-    params = json.load(f)
+    index = params["system"]
+    features = metadata[index]["features"]
+    system = params["system"]
 
-# load oracles
-exec(open("./build/oracles.py").read()) # systems = ...
+    # experiment_repetition_id, not the repetition for each datapoint
+    repetition = params["repetition"]
+    abs_sample_size = max(1, int(params["rel_sample_size"] * features))
 
-records = []
-feature_selections = []
-for repetition in range(REPETITIONS):
+    relevant_terms_level = metadata[index]["relevant_terms_level"]
+    interaction_p = metadata[index]["p_interaction_degree"]
 
-	x = np.random.randint(2, size=(n_configs, n_features))
-	y = np.array(list(map(systems[index), x))
+    relevant_options = []
+    for term in params["system"]["terms"]:
+        relevant_options += term["options"]
 
-    # compute x_diff and x_xor for screening
-    x_diff, y_diff = diff_transform(x, y)
-    x_xor, y_xor = xor_transform(x, y)
+    # load oracles
+    exec(open("./build/oracles.py").read())  # systems = ...
 
-	# compute and store feature selections
-    feature_selection = {
-		'lasso_screen': lasso_screening(x_diff, y_diff),
-		'group_screen': group_screening(x_diff, y_diff),
-		'causal_screen': find_greedy_hitting_set(x_xor, y_xor, threshold=0.1)
-	}
-	feature_selections.append(feature_selection)
+    records = []
+    feature_selections = []
+    for repetition in range(REPETITIONS):
+        x = np.random.randint(2, size=(abs_sample_size, features))
+        y = np.array(list(map(systems[index], x)))
 
-    # compute precision, recall, f1 score for all repetitions
-	metrics = {
-		'precision': {
-			a: precision(relevant_options, feature_selection[a]) for a in feature_selection.keys()
-		},
-		'recall': {
-			a: recall(relevant_options, feature_selection[a]) for a in feature_selection.keys()
-		},
-		'f1_score': {
-			a: f1(relevant_options, feature_selection[a]) for a in feature_selection.keys()
-		},
+        # compute x_diff and x_xor for screening
+        x_diff, y_diff = diff_transform(x, y)
+        x_xor, y_xor = xor_transform(x, y)
+
+        # compute and store feature selections
+        feature_selection = {
+            "lasso_screen": lasso_screening(x_diff, y_diff),
+            "group_screen": group_screening(x_diff, y_diff),
+            "causal_screen": find_greedy_hitting_set(x_xor, y_xor, threshold=0.1),
+        }
+        feature_selections.append(feature_selection)
+
+        # compute precision, recall, f1 score for all repetitions
+        metrics = {
+            "precision": {
+                a: precision(relevant_options, feature_selection[a])
+                for a in feature_selection.keys()
+            },
+            "recall": {
+                a: recall(relevant_options, feature_selection[a])
+                for a in feature_selection.keys()
+            },
+            "f1_score": {
+                a: f1(relevant_options, feature_selection[a])
+                for a in feature_selection.keys()
+            },
+        }
+
+        # store options and classification metrics
+        records.append(metrics)
+
+    # merge everythin into a single dict
+    results = {}
+    results.update(
+        {
+            "system": index,
+            "repetition": repetition,
+            "features": features,
+            "rel_sample_size": params["rel_sample_size"],
+            "abs_sample_size": abs_sample_size,
+            "relevant_terms_level": relevant_terms_level,
+            "interaction_p": interaction_p,
+            "relevant_options": relevant_options,
+            "feature_selection": feature_selections,
+            "metrics": records,
+        }
+    )
+    return results
 
 
-	# store options and classification metrics
-	records.append(metrics)
+if __name__ == "__main__":
+    import sys
 
-# store results to f"./results/{index}.json"
-with open(f"./results/{index}.json", "w+") as f:
-    pass
+    index = sys.argv[1]
+
+    result = main(index)
+
+    # store results
+    with open(f"./results/{index}.json", "w+") as f:
+        f.write(json.dumps(result, indent=2))
