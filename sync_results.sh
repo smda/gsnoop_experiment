7c68f9a26e5cfc820cf185ebc745d115bc828bda
evostevo <asdf@qwertz.ui>
2024-06-02 21:23:55 +0200

    specs

--- sync_results.sh
+++ sync_results.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#<<<<<<< HEAD
-cd /home/stefan/git/gsnoop_experiment/
-#=======
-#cd /home/muehlbauer/git/gsnoop_experiment/
-#>>>>>>> bb65ed6f1de7dcaedbd8cea3ab9eb208a6003aba
+
+cd /home/$USER/git/gsnoop_experiment/
+
 git pull
 git add results/*
 git commit -am 'autocommit'
